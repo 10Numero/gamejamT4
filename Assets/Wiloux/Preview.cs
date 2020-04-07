d7e2b447ec83ee4348339fceb725ea35c983400b
Wiloux <54811449+Wiloux@users.noreply.github.com>
2020-04-07 13:15:47 +0200

    Inv Management

---
+++ Assets/Wiloux/Preview.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Preview : MonoBehaviour
+{
+    public GameObject OriginPic;
+    public GameObject BigPic;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(OriginPic != null)
+        {
+            BigPic.GetComponent<RawImage>().texture = OriginPic.GetComponent<RawImage>().texture;
+        }
+        else
+        {
+            BigPic.GetComponent<RawImage>().texture = null;
+        }
+    }
+
+    public void Delete()
+    {
+       
+        OriginPic.GetComponent<Selected>().isSelected = false;
+        Destroy(OriginPic);
+    }
+
+    public void RemoveIndex()
+    {
+        if (OriginPic.GetComponent<Selected>().index != 0)
+        {
+            OriginPic.GetComponent<Selected>().index--;
+        }
+    }
+    public void AddIndex()
+    {
+        OriginPic.GetComponent<Selected>().index++;
+    }
+}
---
+++ Assets/Wiloux/Selected.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Selected : MonoBehaviour
+{
+    public GameObject Preview;
+    public GameObject Inv;
+    public bool isSelected = false;
+    public int index;
+
+
+    private void Awake()
+    {
+        index = transform.GetSiblingIndex();
+        Preview = GameObject.FindGameObjectWithTag("Preview");
+        Inv = GameObject.FindGameObjectWithTag("Inventory");
+
+    }
+    private void Update()
+    {
+        transform.SetSiblingIndex(index);
+    }
+
+
+    // Update is called once per frame
+
+    public void SelectPic()
+    {
+
+        if (!isSelected)
+        {
+            Preview.SetActive(true);
+            Preview.GetComponent<Preview>().OriginPic = this.gameObject;
+            Preview.GetComponent<Preview>().BigPic.SetActive(true);
+            //transform.parent = Preview.transform;
+            //transform.localPosition = new Vector3(1f, 1f, 1f);
+            //transform.localScale = new Vector3(160f, 100f, 1f);
+            //else if (isSelected)
+            //{
+            //    Preview.GetComponent<Preview>().OriginPic = null;
+            //    Preview.GetComponent<Preview>().BigPic.SetActive(false);
+            //    Preview.SetActive(false);
+            //    //transform.parent = Inv.transform;
+            //    //transform.localScale = new Vector3(1f, 1f, 1f);
+            //    isSelected = false; 
+            //   transform.SetSiblingIndex(index);
+        }
+    }
+}
