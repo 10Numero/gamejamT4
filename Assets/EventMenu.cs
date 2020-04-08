96c4b046a7b72dfd2fd5ea8bfdd031fe4d07088e
10Numero <63287441+10Numero@users.noreply.github.com>
2020-04-08 09:50:27 +0200

    menu update
    update assets sounds mat

---
+++ Assets/EventMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventMenu : MonoBehaviour
+{
+
+    public Animator anim;
+    public AudioSource audio;
+    public AudioClip wood;
+    private bool closer;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void OnMouseOver()
+    {
+        if (closer == false){
+            audio.PlayOneShot(wood, 1);
+            closer = true;
+        }
+
+       
+        anim.SetBool("over_", true);
+    }
+
+    void OnMouseExit()
+    {
+        anim.SetBool("over_", false);
+        closer = false;
+    }
+}
