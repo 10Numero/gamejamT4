bfae288d1b2cbffc4ddf732866659aa1215fb93d
Wiloux <54811449+Wiloux@users.noreply.github.com>
2020-04-08 09:54:55 +0200

    Object highlight

---
+++ Assets/ObjetOfInterest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjetOfInterest : MonoBehaviour
+{
+    public int GameState = 0;
+    public int InterestState = 1;
+
+    public Material InterestMat;
+    public Material NonInterestMat;
+
+    private Renderer rend;
+
+    // Update is called once per frame
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+    void Update()
+    {
+        if(GameState == InterestState)
+        {
+            rend.material = InterestMat;
+        }
+        else
+        {
+            rend.material = NonInterestMat;
+        }
+    }
+}
