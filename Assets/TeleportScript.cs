f9682ca45444aae8879fd912adcff0ebf4de1fb9
10Numero <63287441+10Numero@users.noreply.github.com>
2020-04-10 15:14:45 +0200

    zfce
    fzrefc

--- Assets/TeleportScript.cs
+++ Assets/TeleportScript.cs
@@ -6,6 +6,8 @@ public class TeleportScript : MonoBehaviour
 {
 
     public bool aze;
+    public Transform[] tpPoint;
+    public Transform player;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +18,20 @@ public class TeleportScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void OnTriggerEnter()
+    {
+        if(aze == false)
+        {
+            player.transform.position = tpPoint[0].transform.position;
+            aze = true;
+        }
+        else
+        {
+            player.transform.position = tpPoint[1].transform.position;
+            aze = false;
+        }
     }
 }
